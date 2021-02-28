13e0e503b2ee561cd79860eed42a7cdd4fb1e725
Razvan <razvanbugoi@gmail.com>
2021-02-28 16:39:39 +0000

    finished writing pseudocode for my project

---
+++ script.js
@@ -0,0 +1,28 @@
+// game is gonna be played against computer
+// create a function called 'computerPlay' that will randomly return either 'Rock' , 'Paper' or 'Scissors'
+// Write a function called 'playRound' that plays a single round of Rock Paper Scissors
+// The function should take two parameters(playerSelection, computerSelection) {
+
+    // - make the two parameters lowercase
+    // - return a string that declares the winner of the round
+    // - there can only be 4 possible outcomes:
+    // - 1. "You Lose! Paper beats Rock"
+    // - 2. "You Lose! Scissors beats Paper"
+    // - 3. "You Lose! Rock beats Scissors"
+    // - 4. "It's a tie! You selected the same item."
+    // - "You Lose!" will be replaced by "You Won!" if the playerSelection is the winner;
+// }
+
+// Write a new function called game() {
+
+    // use the computerPlay function inside this one to play a 5 round game
+    // we need 2 variables to keep track of the score for both players
+    // variables should be initiated with 0
+    // if one player wins a round, his variable should increase by 1;
+    // if it's a tie round, both variables will remain the same;
+    // use console.log after each round to display the score;
+    // when one of the variables reaches value = 5, return a message like so 'Player Won' / 'Computer Won'
+// }
+
+// use 'prompt' to get input from the user;
+
